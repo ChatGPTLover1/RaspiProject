769670ad235ce60bf25a8726bbf7a736b5b8fd04
PeterHannes123 <linus.kohlmann@haw-hamburg.de>
2023-06-17 14:16:00 +0200

    Error handling added

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from deutsche_bahn_api.timetable_helper import TimetableHelper
 import telepot
 from telepot.loop import MessageLoop
 
+
 api = ApiAuthentication("4d92b50e16b9e7a97f9a95d55a08570e", "3ac2ae0cd903b5aa9315ebf338addbaf")
 success: bool = api.test_credentials()
-
+Bahnhof = "Hamburg Hbf" # default station
 station_helper = StationHelper()
 station_helper.load_stations()
 # found_stations = station_helper.find_stations_by_lat_long(47.996713, 7.842174, 10)
-found_stations_by_name = station_helper.find_stations_by_name("Hamburg Hbf")
+found_stations_by_name = station_helper.find_stations_by_name(Bahnhof)
 station = found_stations_by_name[0]
 
 timetable_helper = TimetableHelper(station=station, api_authentication=api)
@@ -21,6 +22,27 @@ trains_in_this_hour = timetable_helper.get_timetable()
 
 trains_with_changes = timetable_helper.get_timetable_changes(trains_in_this_hour)
 
+
+
+def updatePlan(Bahnhof):
+    global found_stations_by_name, station, station_helper, timetable_helper, trains_in_this_hour, trains_with_changes
+    station_helper = StationHelper()
+    station_helper.load_stations()
+    # found_stations = station_helper.find_stations_by_lat_long(47.996713, 7.842174, 10)
+    found_stations_by_name = station_helper.find_stations_by_name(Bahnhof)
+    try:
+        station = found_stations_by_name[0]
+
+    except:
+        return 0
+
+    timetable_helper = TimetableHelper(station=station, api_authentication=api)
+    trains_in_this_hour = timetable_helper.get_timetable()
+    # trains_at_given_hour = timetable_helper.get_timetable(12)
+
+    trains_with_changes = timetable_helper.get_timetable_changes(trains_in_this_hour)
+
+    return 1   #no error
 # Hier gehts los mit der Sortierung
 #metronometrains = [train for train in trains_in_this_hour if
 #                   train.train_type == "ME"]  # sortiere  die Liste nach dem Zugtyp Metronom
@@ -75,19 +97,35 @@ def reverse_split(string):
 
 
 def handle_messages(msg):
+    global Bahnhof, found_stations_by_name, station, station_helper, timetable_helper, trains_in_this_hour, trains_with_changes
     content_type, chat_type, chat_id = telepot.glance(
         msg)  # Python-Telegram function that returns the values message type, chat type  and chat id from a telegram message
     if content_type == 'text':  # glance checks the message type. If the type is in the form of text, we have a valid message
         if msg['text'] == 'Züge':  # user input
             for i, train in enumerate(trains_in_this_hour):
-                message = f"Zug {i} \n departure= {reverse_split(train.departure)} \n  platform = {train.platform} \n stations = {train.stations} \n  train_changes = {train.train_changes} \n " \
+                message = f"Zug {i+1} \n departure= {reverse_split(train.departure)} \n  platform = {train.platform} \n stations = {train.stations} \n  train_changes = {train.train_changes} \n " \
                           f"train_number = {train.train_number} \n train_type = {train.train_type} \n "
 
                 bot.sendMessage(telegram_chat_id, message)
 
-        if msg['text'] == 'Moin':  # user is kind
-            message = f"Menü: 1. Züge"  # bot prepares a welcoming message
+        if msg['text'] == 'help':  # user is kind
+            message = f"Menü: 1. Züge \nMenü: 2. Bahnhof"  # bot prepares a welcoming message
             bot.sendMessage(telegram_chat_id, message)  # and sends it
+
+        if msg['text']:
+            message = msg['text']
+            if message.startswith('Bahnhof'):  # set station
+                Bahnhof = message.split(' ', 1)[1]
+                if updatePlan(Bahnhof) ==1:
+                    updatePlan(Bahnhof)
+                    message = "Dein gewählter Bahnhof ist: " + Bahnhof
+                    bot.sendMessage(telegram_chat_id, message)  # and sends it
+                    print(f"  return  = {updatePlan(Bahnhof)}")
+                else:
+                    message = "Fehlerhafte Eingabe. Bitte überprüfe den Namen des Bahnhofs"
+                    bot.sendMessage(telegram_chat_id, message)  # and sends it
+
+
 
 
 # initialize Telegram-Bot
@@ -99,4 +137,4 @@ bot.getUpdates()
 MessageLoop(bot, handle_messages).run_as_thread()
 
 while 1:
-    time.sleep(10)
+    time.sleep(1)
